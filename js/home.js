cdd0b25ab24f2c83cce180e15a9be2e25fd9f150
dipak <dipak@milaap.org>
2019-11-20 21:51:16 +0530

    Added deal of the day section

--- js/home.js
+++ js/home.js
@@ -2,4 +2,11 @@
 
 document.addEventListener("DOMContentLoaded", function () {
   console.log("Page loaded");
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    items: 1,
+    autoplay: true
+  });
 });
